61cdd60ccd4f594c2a48102139dcfc0e1adb504d
Aydın Enes <aydinenes4221@gmail.com>
2023-08-23 04:01:26 +0300

    Add embedding PDF

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from markdown_utility import add_images_to_md, add_images_from_folder, add_images_from_attachments
+from markdown_utility import add_files_to_md, add_files_from_folder, add_files_from_attachments
 
 def main():
     # Ask the user for the vault directory path
     vault_directory = input("Enter the path to your Obsidian vault directory: ")
 
-    # Ask the user which images they want to add
-    option = input("Select an option:\n1. Add images from the same folder\n2. Add images from the 'attachments' folder\nEnter 1 or 2: ")
+    # Ask the user which files they want to add
+    option = input("Select an option:\n1. Add files from the same folder\n2. Add files from the 'attachments' folder\nEnter 1 or 2: ")
 
     if option == '1':
-        add_images_from_folder(vault_directory)
+        add_files_from_folder(vault_directory)
     elif option == '2':
-        add_images_from_attachments(vault_directory)
+        add_files_from_attachments(vault_directory)
     else:
         print("Invalid option. Please select 1 or 2.")
 
--- markdown_utility.py
+++ markdown_utility.py
@@ -1,30 +1,29 @@
 import os
 
-
-def add_images_to_md(md_file, image_paths, vault_directory):
+def add_files_to_md(md_file, file_paths, vault_directory):
     # Read the existing content of the MD file
     with open(md_file, 'r', encoding='utf-8') as file:
         md_content = file.read()
 
-    # Append the Markdown image syntax for each image in the list
-    for image_path in image_paths:
-        relative_path = os.path.relpath(image_path, vault_directory)
+    # Append the Markdown file link for each file in the list
+    for file_path in file_paths:
+        relative_path = os.path.relpath(file_path, vault_directory)
         md_content += f'\n![[{relative_path}]]\n'
 
     # Write the updated content back to the MD file
     with open(md_file, 'w', encoding='utf-8') as file:
         file.write(md_content)
 
-def add_images_from_folder(vault_directory):
+def add_files_from_folder(vault_directory):
     # Iterate through the directories within the vault directory
     for root, _, _ in os.walk(vault_directory):
         for file in os.listdir(root):
             if file.endswith('.md'):
                 md_file_path = os.path.join(root, file)
-                image_files = [os.path.join(root, f) for f in os.listdir(root) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg'))]
-                add_images_to_md(md_file_path, image_files, vault_directory)
+                supported_files = [os.path.join(root, f) for f in os.listdir(root) if f.lower().endswith(('.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg'))]
+                add_files_to_md(md_file_path, supported_files, vault_directory)
 
-def add_images_from_attachments(vault_directory):
+def add_files_from_attachments(vault_directory):
     attachments_folder = os.path.join(vault_directory, 'attachments')
 
     if not os.path.exists(attachments_folder):
@@ -34,7 +33,7 @@ def add_images_from_attachments(vault_directory):
     # Iterate through the directories within the attachments folder
     for root, _, _ in os.walk(attachments_folder):
         for file in os.listdir(root):
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg')):
+            if file.lower().endswith(('.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg')):
                 # Extract the filename prefix (Markdown file name)
                 filename_prefix = file.split('_', 1)[0]
 
@@ -43,5 +42,5 @@ def add_images_from_attachments(vault_directory):
                     for file2 in files2:
                         if file2.endswith('.md') and file2.startswith(filename_prefix):
                             md_file_path = os.path.join(root2, file2)
-                            image_path = os.path.join(root, file)
-                            add_images_to_md(md_file_path, [image_path], vault_directory)
+                            file_path = os.path.join(root, file)
+                            add_files_to_md(md_file_path, [file_path], vault_directory)
